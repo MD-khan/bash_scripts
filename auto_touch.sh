2b549ba9eae3dba0672f395d1cad3b03a44a8762
MD-khan <md.monir.khan707@gmail.com>
2022-01-21 23:25:26 -0500

    Remove the if statement
    To make this script, I removed the if statement that was not necessary to run this script

--- auto_touch.sh
+++ auto_touch.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-# Find total number of files in the firectory
+# Define the max number of file 
+max_number=2
+count=0
+
+# Count and assined total number of files in the firectory
 total_files=`ls -1 | wc -l`
 
-# Ecxclude the this script file from the total number of files
+# Ecxclude the script file from the total number of files
 total_files="$(($total_files-1))"
 
-# If the inital total files count is less than 25
-# We need to update files count
+# Update the max_number
+max_number="$(($total_files+2))"
 
-#counter=0
-#last_number=0
-max_number=25
-
-if (( max_number <= total_files))
-then
-    max_number="$(($total_files+25))"
-echo $max_number
-fi
-
-while [ $total_files -lt $max_number ]
-do
-    `touch mdKhan_$total_files`
-    ((total_files++))
+while [ $count != $max_number ]
+do   
+    `echo "Hello There I'm file number $count" > mdKhan_$count.txt`  
+    ((count++))
 done
-
-echo $total_files
